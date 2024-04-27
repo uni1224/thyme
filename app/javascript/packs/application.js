ab8d3e3ae68d3b0d6913841484c1fc97bd8ff278
uni1224 <saekahane99@gmail.com>
2024-04-28 00:20:28 +0900

    レイアウト変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,5 +4,6 @@ import "bootstrap";
 import "../stylesheets/application"; 
 import '../stylesheets/mystyle.css'
 import "@hotwired/turbo-rails"
+import "channels"
 //= require jquery
 //= require jquery_ujs
